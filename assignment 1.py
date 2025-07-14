0f0c9b5cd162cb5089c2bad7d4045c616d0cae97
arunrajwal <arunrajwal24@gmail.com>
2025-07-14 19:26:58 +0530

    Add files via upload
    Assignment files for the python course

---
+++ assignment 1.py
@@ -0,0 +1,10 @@
+a = input("Enter the first number: ")
+a= int(a)
+
+b = input("Enter the second number :  ")
+b = int(b)
+
+print("\n \n ADDITION :  ", a+b)
+print("SUBTRACTION :  ", a-b)
+print("MULTIPLICATION :  ", a*b)
+print("DIVISION :  ", a/b)
